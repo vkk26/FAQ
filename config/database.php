bed1ac0cb600e0c4ce3a38a8126780f5cbc84395
Vkk26 <vkk26@njit.edu>
2018-11-20 06:01:14 -0500

    modified database

--- config/database.php
+++ config/database.php
@@ -82,6 +82,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
         'pg-heroku' => [
             'driver' => 'pgsql',
             'host' => $heroku_db_url['host'],
